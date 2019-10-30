60fc54ded0e3854e33c0c86fecc7d8d279c08f2b
yusufnafey <yusufnafey@yahoo.com>
2019-10-30 14:57:18 -0700

    creates classes

--- src/adv.py
+++ src/adv.py
@@ -34,3 +34,6 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+name = input("Enter your name: ")
+print(name)
